86cbf586de34b61d472a9587888b6ecfbd0f53fc
Jakub Bednar <jakub.bednar@gmail.com>
2024-01-31 20:30:28 +0100

    feat: add Promotion text

--- src/components/promotion.tsx
+++ src/components/promotion.tsx
@@ -1,8 +1,19 @@
 import * as React from "react";
 import {useQueryConfig} from "../data/queries";
+import {graphql, useStaticQuery} from "gatsby";
 
 export const Promotion: React.FC = () => {
     let config = useQueryConfig();
+    const markdown = useStaticQuery(graphql`
+        query {
+            markdownRemark(fileAbsolutePath: {regex: "/promotion/"}) {
+                html
+                frontmatter {
+                    title
+                }
+            }
+        }`
+    ).markdownRemark;
     return (
         <section>
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
@@ -16,17 +27,11 @@ export const Promotion: React.FC = () => {
                     </div>
 
                     <div className="lg:py-24">
-                        <h2 className="text-3xl font-bold sm:text-4xl">Grow your audience</h2>
+                        <h2 className="text-3xl font-bold sm:text-4xl">{markdown.frontmatter.title}</h2>
+                        <p className="mt-4 text-gray-600" dangerouslySetInnerHTML={{__html: markdown.html}}/>
 
-                        <p className="mt-4 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-                            eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
-                            quidem quam repellat.
-                        </p>
-
-                        <a
-                            href="/"
-                            className={`mt-8 inline-block rounded bg-${config.primaryColor} px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400`}
+                        <a href="/"
+                           className={`mt-8 inline-block rounded bg-${config.primaryColor} px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400`}
                         >
                             Get Started Today
                         </a>
---
+++ src/data/content/promotion.md
@@ -0,0 +1,5 @@
+---
+title: "Promotion Section"
+---
+
+Please set your promotion section here: `src/data/content/promotion.md`
