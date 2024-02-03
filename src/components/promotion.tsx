3e93a35dfc5011b95a439aca6aeedb1bf89b26e3
Jakub Bednar <jakub.bednar@gmail.com>
2024-02-03 12:53:03 +0100

    fix: styling

--- src/components/promotion.tsx
+++ src/components/promotion.tsx
@@ -21,7 +21,7 @@ export const Promotion: React.FC = () => {
     return (
         <section>
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-                <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+                <div className="grid grid-cols-1 gap-8 lg:grid-cols-[2fr,1fr] lg:gap-16">
                     <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
                         <img src={AppScreenshot} alt="Screenshot of the App" className="absolute inset-0 h-full w-auto object-cover"/>
                     </div>
